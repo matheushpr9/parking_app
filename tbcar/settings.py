16fc0ea56e785f4fd03200477449f4626b11dd94
MatheusPtasinski <matheus.ptasinski@dafiti.com.br>
2023-06-15 22:27:12 -0300

    fix

--- tbcar/settings.py
+++ tbcar/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'media_files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_files')
